365ea059cbe4588ef7d7cf7a7af09c25e1cdb370
Fatemeh-ledari <fatemehzahra.asgharzadehledari@dawsoncollege.qc.ca>
2024-09-13 23:17:24 +0000

    activity 2

---
+++ Tax.py
@@ -0,0 +1,7 @@
+price = float(input("Enter the price of the food"))
+priceTaxInculded = 0.18 + price
+print(" This is the price tax icluded:", priceTaxInculded)
+
+
+x = "fatemeh" *3 
+print(x)
---
+++ function.py
@@ -0,0 +1,11 @@
+circule1 = 3.14 * 3 ** 2 
+circule2 = 3.14 * 5 ** 2
+circule3 = 3.14 * 10 ** 2
+
+
+def calculateArea(radius):
+    Area = 3.14 * radius **2
+    return Area
+
+circule1 = calculateArea(3)
+print(circule1)
